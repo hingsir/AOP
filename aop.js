bcb258703b19aaf77c0b97498ffed8635f8498ef
chexingyou <chexingyou@anhouse.com>
2015-12-22 16:30:07 +0800

    refactor

--- aop.js
+++ aop.js
@@ -1,73 +1,68 @@
-;(function(global,undefined){
+;
+(function(global, undefined) {
 
-    var AOP = {};
+    var AOP = {},
+        beforeAdvices = [],
+        afterAdvices = [],
+        _getBeforeAdvice = _getAdvice(beforeAdvices),
+        _getAfterAdvice = _getAdvice(afterAdvices);
 
-    Function.prototype.exec = function(){
+    Function.prototype.exec = Function.prototype.invoke = function() {
         var self = this,
             funcName = self.name,
             args = [].slice.call(arguments),
-            beforeAdvice = AOP.getBeforeAdvice(funcName),
-            afterAdvice = AOP.getAfterAdvice(funcName),
+            beforeAdvice = _getBeforeAdvice(funcName),
+            afterAdvice = _getAfterAdvice(funcName),
             ret;
-        if(beforeAdvice){
-            ret = beforeAdvice.apply(self,arguments)
-            if(ret === undefined){
-                ret = self.apply(self,arguments);
-            }
-        }else{
-            ret = self.apply(self,arguments);
+        if (beforeAdvice) {
+            ret = beforeAdvice.apply(self, arguments)
+            ret === undefined && (ret = self.apply(self, arguments));
+        } else {
+            ret = self.apply(self, arguments);
         }
 
-        if(afterAdvice){
+        if (afterAdvice) {
             args.unshift(ret);
-            var afterRet = afterAdvice.apply(self,args);
-            if(afterRet !== undefined){
-                return afterRet;
-            }else{
-                return ret;
-            }
-        }else{
+            var afterRet = afterAdvice.apply(self, args);
+            return afterRet !== undefined ? afterRet : ret;
+        } else {
             return ret;
         }
     }
 
-    AOP.beforeAdvices = [];
-    AOP.afterAdvices = [];
-    AOP.before = function(pointcut,beforeAdvice){
-        AOP.beforeAdvices.push({
-            pointcut:pointcut,
-            advice:beforeAdvice
+    AOP.before = function(pointcut, beforeAdvice) {
+        beforeAdvices.push({
+            pointcut: pointcut,
+            advice: beforeAdvice
         });
     }
-    AOP.after = function(pointcut,afterAdvice){
-        AOP.afterAdvices.push({
-            pointcut:pointcut,
-            advice:afterAdvice
+    AOP.after = function(pointcut, afterAdvice) {
+        afterAdvices.push({
+            pointcut: pointcut,
+            advice: afterAdvice
         });
     }
-    AOP.around = function(pointcut,beforeAdvice,afterAdvice){
-        AOP.beforeAdvices.push({
-            pointcut:pointcut,
-            advice:beforeAdvice
+    AOP.around = function(pointcut, beforeAdvice, afterAdvice) {
+        beforeAdvices.push({
+            pointcut: pointcut,
+            advice: beforeAdvice
         });
-        AOP.afterAdvices.push({
-            pointcut:pointcut,
-            advice:afterAdvice
+        afterAdvices.push({
+            pointcut: pointcut,
+            advice: afterAdvice
         });
     }
-    AOP.getBeforeAdvice = _getAdvice(AOP.beforeAdvices);
-    AOP.getAfterAdvice = _getAdvice(AOP.afterAdvices);
 
-    function _getAdvice(advices){
-        return function(funcName){
-            for(var i in advices){
+    function _getAdvice(advices) {
+        return function(funcName) {
+            for (var i in advices) {
                 var pointcut = advices[i].pointcut;
-                if(pointcut instanceof RegExp){
-                    if(pointcut.test(funcName)){
+                if (pointcut instanceof RegExp) {
+                    if (pointcut.test(funcName)) {
                         return advices[i].advice;
                     }
-                }else{
-                    if(pointcut == funcName){
+                } else {
+                    if (pointcut == funcName) {
                         return advices[i].advice;
                     }
                 }
@@ -77,7 +72,7 @@
 
     if (typeof module !== 'undefined' && module && typeof module.exports !== 'undefined') {
         module.exports = AOP;
-    }else{
+    } else {
         global.AOP = AOP;
     }
 
